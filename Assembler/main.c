132900902beb4c9a2fe046d1acd371ba02b344ef
Yotam Aharony <yotamaharony@gmail.com>
2018-12-03 09:23:05 +0200

    fix label issues
    fix labels

--- Assembler/main.c
+++ Assembler/main.c
@@ -22,18 +22,16 @@ void readFile();
 List* get_labels_lines( char* filename);
 char opcode_to_bin(char* opcode);
 char registerName_to_number(char* name);
-
-//NO NEED
-char* opcode_to_bin_CHAR(char* opcode);
-char* registerName_to_number_CHAR(char* name);
+int imm_to_hex(char* imm);
+void label_to_hex(char* str, int imm);
 
 int main(int argc, char** argv) 
 {
 	char* filename = "C:\\Users\\Yotam\\Documents\\fib.asm";
 	//get_labels_lines(labels, filename);
-	List* labels = get_labels_lines(filename);
+	//List* labels = get_labels_lines(filename);
 
-	printf("\n");
+	//printf("\n");
 	readFile();
 	return 0;
 }
@@ -66,22 +64,23 @@ List* get_labels_lines( char* filename) {
 			if (strchr(comm[0], ':'))
 			{
 				//labels[j] = comm[0];
-				addNode(labels, comm[0], j);
+				addNode(labels, comm[0], j-1);
 			}
 
 			j++;
 		}
 	}
 
+	/*
 	Node* temp = labels->head;
 	while (temp)
 	{
 		printf("%s:%d\n", temp->label_name, temp->line_number);
 		temp = temp->next;
 	}
-
-
+	*/
 	fclose(fp);
+	return labels;
 }
 
 void readFile()
@@ -92,6 +91,9 @@ void readFile()
 	char delimiters[] = " \t\r\n,";
 	char* filename = "C:\\Users\\Yotam\\Documents\\fib.asm";
 	int i;
+	char opcode, rd, rs, rm, rt;
+	//char imm[3];
+	char str[3] = "000";
 	List* labels = get_labels_lines(filename);
 
 	fp = fopen(filename, "r");
@@ -128,11 +130,42 @@ void readFile()
 			/* currently not working correctly */
 			int find = find_node(labels, comm[5]);
 			if (find != -1)
-				comm[5] = find;
+			{
+				label_to_hex(str, find);
+				//printf("new try: %04x\n", find);
+				//printf("%s: %d %s ", comm[5], find, str);
+				comm[5] = str;
+			}
+			else
+			{// convert the imm to hex
+				itoa(atoi(comm[5]), str, 16);
+				comm[5] = str;
+				//label_to_hex(str, atoi(comm[5]));
+			}
+
+			
 
 			if (!strchr(comm[0], '.'))
+			{
+				//printf("LINE#%d: opcde=%s, rd=%s, rs=%s, rt=%s, rm=%s, imm=%s       ", j, comm[0], comm[1], comm[2], comm[3], comm[4], comm[5]);
+				opcode = opcode_to_bin(comm[0]);
+				rd = registerName_to_number(comm[1]);
+				rs = registerName_to_number(comm[2]);
+				rt = registerName_to_number(comm[3]);
+				rm = registerName_to_number(comm[4]);
+				//imm = comm[5];
+				
+				int imm_size = strlen(comm[5]);
+				if (imm_size == 1)
+					printf("%c%c%c%c%c00%s\n", opcode, rd, rs, rt, rm, strupr(comm[5]));
+				else if (imm_size == 2)
+					printf("%c%c%c%c%c0%s\n", opcode, rd, rs, rt, rm, strupr(comm[5]));
+				else
+					printf("%c%c%c%c%c%s\n", opcode, rd, rs, rt, rm, strupr(comm[5]));
+				
 				//printf("%c%c%c%c%c0000\n", opcode_to_bin(comm[0]), registerName_to_number(comm[1]), registerName_to_number(comm[2]), registerName_to_number(comm[3]), registerName_to_number(comm[4]));
-				printf("%c%c%c%c%c%04d\n", opcode_to_bin(comm[0]), registerName_to_number(comm[1]), registerName_to_number(comm[2]), registerName_to_number(comm[3]), registerName_to_number(comm[4]), atoi(comm[5]));
+				//printf("%c%c%c%c%c%04d\n", opcode_to_bin(comm[0]), registerName_to_number(comm[1]), registerName_to_number(comm[2]), registerName_to_number(comm[3]), registerName_to_number(comm[4]), atoi(comm[5]));
+			}
 			else
 			{
 				/* do .word*/
@@ -150,6 +183,22 @@ void readFile()
 	fclose(fp);
 }
 
+int imm_to_hex(char* imm) {
+	if (strstr(imm, "0x") != NULL) { // Num is in hexadecimal
+		return (int)strtol(imm, NULL, 0);
+	}
+	return strtol(imm, NULL, 10);
+}
+
+void label_to_hex(char* str, int imm) {
+	itoa(imm, str, 16);
+	int size = strlen(itoa);
+	if (strlen == 1)
+		//str = strcat("00", str);
+		strcat(str, "00");
+	else if (strlen == 2)
+		str = strcat("0", str);
+}
 
 char opcode_to_bin(char* opcode)
 {   
@@ -194,17 +243,17 @@ char opcode_to_bin(char* opcode)
 
 char registerName_to_number(char* name)
 {
-	if (strcmp(name, "$zero") == 0) //0 = equal
+	if (strcmp(name, "$zero") == 0 || strcmp(name, "0") == 0) //0 = equal
 		return '0';
-	else if (strcmp(name, "$at") == 0)
+	else if (strcmp(name, "$at") == 0 || strcmp(name, "1") == 0)
 		return '1';
-	else if (strcmp(name, "$v0") == 0)
+	else if (strcmp(name, "$v0") == 0 || strcmp(name, "2") == 0)
 		return '2';
-	else if (strcmp(name, "$a0") == 0)
+	else if (strcmp(name, "$a0") == 0 || strcmp(name, "3") == 0)
 		return '3';
-	else if (strcmp(name, "$a1") == 0)
+	else if (strcmp(name, "$a1") == 0 || strcmp(name, "4") == 0)
 		return '4';
-	else if (strcmp(name, "$t0") == 0)
+	else if (strcmp(name, "$t0") == 0 || strcmp(name, "5") == 0)
 		return '5';
 	else if (strcmp(name, "$t1") == 0)
 		return '6';
@@ -225,87 +274,7 @@ char registerName_to_number(char* name)
 	else if (strcmp(name, "$fp") == 0)
 		return 'E';
 	else if (strcmp(name, "$ra") == 0)
-		return "F";
-
-	return "!";
-}
-
-/* this function can be removed */
-char* opcode_to_bin_CHAR(char* opcode)
-{
-	if (strcmp(opcode, "add") == 0) //0 = equal
-		return "0000";
-	else if (strcmp(opcode, "sub") == 0)
-		return "0001";
-	else if (strcmp(opcode, "and") == 0)
-		return "0002";
-	else if (strcmp(opcode, "or") == 0)
-		return "0003";
-	else if (strcmp(opcode, "sll") == 0)
-		return "0004";
-	else if (strcmp(opcode, "sra") == 0)
-		return "0005";
-	else if (strcmp(opcode, "mac") == 0)
-		return "0006";
-	else if (strcmp(opcode, "branch") == 0)
-		return "0007";
-	else if (strcmp(opcode, "reserved") == 0)
-		return "0008";
-	/*
-	else if (strcmp(opcode, "sub"))
-	return 0x9;
-	else if (strcmp(opcode, "sub"))
-	return 0xa;
-	*/
-	else if (strcmp(opcode, "jal") == 0)
-		return "0011";
-	else if (strcmp(opcode, "lw") == 0)
-		return "0012";
-	else if (strcmp(opcode, "sw") == 0)
-		return "0013";
-	else if (strcmp(opcode, "jr") == 0)
-		return "0014";
-	else if (strcmp(opcode, "halt") == 0)
-		return "0015";
-
-	return "!";
-}
-
-/* this function can be                     removed */
-char* registerName_to_number_CHAR(char* name)
-{
-	if (strcmp(name, "$zero") == 0) //0 = equal
-		return "0000";
-	else if (strcmp(name, "$at") == 0)
-		return "0001";
-	else if (strcmp(name, "$v0") == 0)
-		return "0002";
-	else if (strcmp(name, "$a0") == 0)
-		return "0003";
-	else if (strcmp(name, "$a1") == 0)
-		return "0004";
-	else if (strcmp(name, "$t0") == 0)
-		return "0005";
-	else if (strcmp(name, "$t1") == 0)
-		return "0006";
-	else if (strcmp(name, "$t2") == 0)
-		return "0007";
-	else if (strcmp(name, "$t3") == 0)
-		return "0008";
-	else if (strcmp(name, "$s0") == 0)
-		return "0009";
-	else if (strcmp(name, "$s1") == 0)
-		return "0010";
-	else if (strcmp(name, "$s2") == 0)
-		return "0011";
-	else if (strcmp(name, "$gp") == 0)
-		return "0012";
-	else if (strcmp(name, "$sp") == 0)
-		return "0013";
-	else if (strcmp(name, "$fp") == 0)
-		return "0014";
-	else if (strcmp(name, "$ra") == 0)
-		return "0015";
+		return 'F';
 
 	return "!";
 }
